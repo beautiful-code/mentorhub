3dd0caf9461d7aa2b2ac80eacb6573d4d496a2ec
Rushil Basappa <rushil.oucse37@gmail.com>
2016-06-07 14:51:46 +0530

    Updated data models

--- db/migrate/20160606084411_create_track_instance.rb
+++ db/migrate/20160606084411_create_track_instance.rb
@@ -2,6 +2,7 @@ class CreateTrackInstance < ActiveRecord::Migration
   def change
     create_table :track_instances do |t|
       t.string :name
+      t.string :type
       t.integer :mentor_id
 
       t.integer :mentoring_track_id
