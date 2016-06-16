346fd9e38d636fda9d231098e55ca6c76f4e26f8
sashank1995 <challasashank@gmail.com>
2016-06-16 19:34:53 +0530

    Done till 2nd form in multistep-form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,5 @@ module ApplicationHelper
   def options_for_types
     [["Select type",""],"Exercise", "Course"]
   end
+
 end
--- app/models/section_interaction.rb
+++ app/models/section_interaction.rb
@@ -2,5 +2,6 @@ class SectionInteraction < ActiveRecord::Base
   has_many :todos
   belongs_to :track_instance
 
-  serialize :resources, Array
+  serialize :resources, Hash
+
 end
