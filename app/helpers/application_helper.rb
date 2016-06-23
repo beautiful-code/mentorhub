836a9c13125ae12dbcb52b347c1bc9db6ee4fda6
Rushil Basappa <rushil.oucse37@gmail.com>
2016-06-23 14:04:06 +0530

    Updated views as in mocks, Added first/last name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    image_tag(gravatar_url, alt: user.first_name, class: "gravatar")
   end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,9 @@ class User < ActiveRecord::Base
     data = access_token.info
     user = User.where(:email => data["email"]).first
     unless user
-      user = User.create(name: data["name"],
+      byebug
+      user = User.create(first_name: data["first_name"],
+                         last_name: data["last_name"],
                          email: data["email"],
                          password: Devise.friendly_token[0,20],
                          image: data["image"]
