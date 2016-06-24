a49b084e30f06ac5c07b82577ca48faece38511d
Revanth <rev4nth@gmail.com>
2016-06-24 13:42:25 +0530

    active_navbar links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,5 +10,15 @@ module ApplicationHelper
     image_tag(gravatar_url, alt: user.first_name, class: "gravatar")
   end
 
-
+  # Given the options, this method determines whether the css class 'active' can be applied to the caller in the sidenav.
+  # options[:c] - controller
+  # options[:a] - action
+  # options[:id] - id of an object
+  def active_class options
+    active =
+      (!options[:c] || (options[:c].include? params[:controller])) &&
+      (!options[:a] || (options[:a].include? params[:action])) &&
+      (!options[:id] || (options[:id].to_s == params[:id]))
+    active ? 'selected' : false
+  end
 end
