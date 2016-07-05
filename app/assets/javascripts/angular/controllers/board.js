86907b50c974a5fdfe1d31038ea1b661e5058a97
Vishal Sharma <vish.sharma0203@gmail.com>
2016-07-05 15:00:30 +0530

    template update for boards

--- app/assets/javascripts/angular/controllers/board.js
+++ app/assets/javascripts/angular/controllers/board.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('mentorhub.board', ['ngRoute'])
+angular.module('mentorhub.board', ['ngRoute', 'duScroll'])
 
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/board', {
