d362697ab136bb7eafa8fb206d2256faebbaa26f
sairam49 <psairam49@gmail.com>
2016-08-17 13:45:55 +0530

    made all the changes mentioned

--- app/assets/javascripts/angular/controllers/learning_track_show.js
+++ app/assets/javascripts/angular/controllers/learning_track_show.js
@@ -1,47 +1,45 @@
 angular.module("mentorhub.learning_track_show", [])
+
     .directive('learningSectionInteractions', function () {
         return {
             templateUrl: '/templates/learning-section-interaction-template.html'
         }
     })
+
     .directive('learningTrack',function(){
-    return {
-      templateUrl: '/templates/learning-track.html'
-    }
+        return {
+           templateUrl: '/templates/learning-track.html'
+        }
     })
 
-    .controller('LearningTrackShowController', ["$scope","$location","BoardServices", "SectionInteractionServices",
-        function ($scope,$location, BoardServices, SectionInteractionServices) {
-            $scope.sectionInteractionServices = SectionInteractionServices;
-            $scope.boardServices = BoardServices;
+    .controller('LearningTrackShowController', ['$scope', '$location', 'BoardServices', 'SectionInteractionServices',
+        function ($scope, $location, BoardServices, SectionInteractionServices) {
 
             var init = function () {
                 $scope.track = LearningTrackShowConfig.track;
                 $scope.track.sections = LearningTrackShowConfig.sections;
             };
 
-
-             $scope.add_mentee_notes = function (sectionInteraction, note) {
+            $scope.add_mentee_notes = function (sectionInteraction, note) {
                 var route_params = {
                     '{track_id}': sectionInteraction.track_id,
                     '{section_id}': sectionInteraction.id
-                };
+              };
 
-                BoardServices.update_section(route_params, {section_interaction: {mentee_notes: note.mentee_notes}})
+             BoardServices.update_section(route_params, {section_interaction: {mentee_notes: note.mentee_notes}})
                     .success(function (response) {
                         sectionInteraction.mentee_notes = note.mentee_notes;
                         note.edit = false;
                         note.mentee_notes = null;
                         window.location = $location.absUrl();
-
                     })
                     .error(function (error) {
                         console.log(error);
                     });
-            };
+              };
 
-             $scope.update_my_todo = function (section, todo) {
-                if (todo.state != 'completed') {
+              $scope.update_my_todo = function (section, todo) {
+                 if (todo.state != 'completed') {
                     var new_state = todo.state == 'incomplete' ? 'to_be_reviewed' : 'incomplete';
 
                     var route_params = {
@@ -51,9 +49,8 @@ angular.module("mentorhub.learning_track_show", [])
                     };
 
                     SectionInteractionServices.update_todo(route_params, {todo: {state: new_state}}, todo);
-                }
-            };
-
+                 }
+              };
 
             init();
         }]);
