e1870a5e830d548fbf5e4357ae3ed1aac7d0c8b3
Pradeep G <itsprdp@gmail.com>
2016-09-12 16:32:51 +0530

    Sendgrid domain whitelist

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,17 +84,16 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-
   config.action_mailer.delivery_method = :smtp
   host = 'mentorhub-beta.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
+
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
     :user_name      => ENV['USERNAME'],
     :password       => ENV['PASSWORD'],
-    :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
 
